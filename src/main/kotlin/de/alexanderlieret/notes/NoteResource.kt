da211f605b37e28e34c9a5c38af0ca844f37ee38
Alexander Lieret <alexander.lieret@fau.de>
2021-10-17 13:42:41 +0200

    Add comments and debug output

--- src/main/kotlin/de/alexanderlieret/notes/NoteResource.kt
+++ src/main/kotlin/de/alexanderlieret/notes/NoteResource.kt
@@ -18,19 +18,25 @@ class NoteResource(
     @GetMapping("/notes")
     fun index(): CollectionModel<EntityModel<Note>> {
         log.info("GET /notes")
+
         val notes = service.findNotes()
-        log.info("Found ${notes.size} notes")
+        log.debug("Found ${notes.size} notes")
+
         return assembler.toCollectionModel(notes)
     }
 
     @PostMapping("/notes")
-    fun post(@RequestBody input: Note): EntityModel<Note> {
+    fun add(@RequestBody input: Note): EntityModel<Note> {
         var note = input
         log.info("POST /notes $note")
 
+        // if the note id is already in use, then generate a new id
         if (note.id?.let { service.get(it).isPresent } == true) note = note.copy(id = UUID.randomUUID())
 
+        // update version
         note = note.copy(version = version())
+        log.debug("new note $note")
+
         service.post(note)
         return assembler.toModel(note)
     }
@@ -40,6 +46,8 @@ class NoteResource(
         log.info("GET /notes/$id")
 
         val note = service.get(id).orElseThrow { NoteNotFoundException(id) }
+        log.debug("requested note $note")
+
         return assembler.toModel(note)
     }
 
@@ -47,11 +55,17 @@ class NoteResource(
     fun update(@RequestBody update: Note, @PathVariable id: UUID): EntityModel<Note> {
         log.info("PUT /notes/$id $update")
 
+        // check if request id and payload id match
         if ((update.id != null) && (update.id != id)) throw IdNotMatchException(id, update)
 
+        // check if note exists
         val note = service.get(id).orElseThrow { NoteNotFoundException(id) }
+        log.debug("old version $note")
 
+        // merge and update version
         val updated = note.merge(update.copy(version = version()))
+        log.debug("merged version $updated")
+
         service.post(updated)
         return assembler.toModel(updated)
     }
@@ -60,7 +74,10 @@ class NoteResource(
     fun delete(@PathVariable id: UUID) {
         log.info("DELETE /notes/$id")
 
+        // check if note does exist
         val note = service.get(id).orElseThrow { NoteNotFoundException(id) }
-        note.id?.let { service.delete(it) }
+        log.debug("delete note $note")
+
+        service.delete(note.id!!)
     }
 }
